1cde1aac64ec4cb74775843515827b112fe90eee
Arif <hossaininfo@ymail.com>
2021-05-28 18:00:37 +0200

    Added Server and Client Example

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -9,8 +11,21 @@ public class Client {
             Socket socket = new Socket("localhost",80);
 
             var output = new PrintWriter(socket.getOutputStream());
-            output.println("Hello from client!");
+            output.println("Hello from client(Arif)!\r\n\r\n");
+
+            var inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output.flush();
+
+            while (true) {
+                var line = inputFromServer.readLine();
+                if (line == null || line.isEmpty()) {
+                    break;
+                }
+                System.out.println(line);
+            }
+
+            inputFromServer.close();
+            socket.close();
             output.close();
 
         } catch (IOException e) {
--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,23 +1,36 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) {
+        //localhost 127.0.0.1
+        //178.174.162.51
 
         try (ServerSocket serverSocket = new ServerSocket(80)) {
 
             while (true) {
                 Socket client = serverSocket.accept();
                 System.out.println(client.getInetAddress());
+
                 var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-                inputFromClient.lines().forEach(System.out::println);
+                while (true) {
+                    var line = inputFromClient.readLine();
+                    if (line == null || line.isEmpty()) {
+                        break;
+                    }
+                    System.out.println(line);
+                }
+
+
+                var outputToClient = new PrintWriter(client.getOutputStream());
+                outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+
+                outputToClient.flush();
                 inputFromClient.close();
+                outputToClient.close();
                 client.close();
             }
 //            String input = "";
