7fddedd0784c7ae96e06d196d471f9acf058c384
Arif <hossaininfo@ymail.com>
2021-05-28 16:50:54 +0200

    Added Server and Client Example

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,10 +1,35 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-        var utils = new Utils();
 
-        System.out.println(utils.message());
+        try (ServerSocket serverSocket = new ServerSocket(80)) {
+
+            while (true) {
+                Socket client = serverSocket.accept();
+                System.out.println(client.getInetAddress());
+                var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                inputFromClient.lines().forEach(System.out::println);
+                inputFromClient.close();
+                client.close();
+            }
+//            String input = "";
+//            while (input != null) {
+//                input = inputFromClient.readLine();
+//                System.out.println(input);
+//            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-
 
 }
